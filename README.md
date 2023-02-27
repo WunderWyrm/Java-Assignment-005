744ec013ff7eb203faeb7b127b44f75a9a53d87a
WunderWyrm <samueltheiss707@gmail.com>
2023-02-26 19:32:50 -0800

    Completed Java Assignment 005

--- README.md
+++ README.md
@@ -4,20 +4,33 @@
 As a new Java developer, you will be required to familiarize yourself with existing code before beginning to develop. That's what this section is about, developing your code tracing skills and familiarizing yourself with parts you may be able to use.
 * Scan through all the existing code and note what you have to work with.
     * Method names
+      * convertC2F (Celsius to Fahrenheit), convertC2K (Celsius to Kelvin), convertF2C(Fahrenheit to Celsius), convertF2K(Fahrenheit to Kelvin), convertK2C(Kelvin to Celsius), getTemp(), getUnitChoice().
     * Variables
+      * input, a scanner variable linked to a scanner object.
 * Using Java terminology, break down every piece of the method **convertF2K**.
     * What is the access modifier?
+      * public
     * Is it a class or object method, how do you know?
+      * Class, it has a static modifier
     * What is its return type?
+      * double
     * What parameters does it require, and what are the parameter(s) datatype(s)?
+      * a single double representing fahrenheit.
     * Describe for me how the body of the method executes (i.e. its flow of execution).
+      * returns the expression convertC2K(convertF2C(fahrenheit)). This makes it convert the argument to Celsius, then from the converted double into kelvin and then returns the double that was converted into kelvin.  
 * Using Java terminology, break down method **getUnitChoice**.
     * What is the access modifier?
+      * public
     * Is it a class or object method, how do you know?
+      * class, it uses the static modifier.
     * What is its return type?
+      * String
     * What parameters does it require, and what are the parameter(s) datatype(s)?
+      * none.
     * Describe for me how the body of the method executes (i.e. its flow of execution).
+      * uses four printf functions to print out a selection screen for four different options. After that it returns a nextLine input.
     * How do the format specifiers differ than what you've seen before and what do they do?
+      * They are specifiers for Strings, but they use #s before the s to denote how much space before or after the String should be made. It seems to make around half the amount of space characters then the number in the specifier.
 
 ## Part 2: - Implement Missing Method: convertK2F
 This Part is aimed at teaching you to use methods and code you have to add new functionality to existing code. In this case you will add the missing conversion method, but you don't need to know math :-)
--- src/TempConversion.java
+++ src/TempConversion.java
@@ -1,3 +1,12 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Samuel Theiss
+ *
+ * @since Version 1.0
+ *
+ */
+
 import java.util.Scanner;
 
 public class TempConversion {
@@ -23,15 +32,27 @@ public class TempConversion {
         return kelvin - 273.15;
     }
 
+    public static double convertK2F(double kelvin) {
+        return convertC2F(convertK2C(kelvin));
+    }
+
     public static double getTemp(String unit) {
-        System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit));
+        System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit.toUpperCase()));
         return Double.parseDouble(input.nextLine());
     }
 
     public static String getUnitChoice() {
-        System.out.printf("%-4s : %40s%n", "C).", "To convert a °C temperature");
-        System.out.printf("%-4s : %40s%n", "F).", "To convert a °F temperature");
-        System.out.printf("%-4s : %40s%n", "K).", "To convert a °K temperature");
+        System.out.printf("%-4s : %40s%n", "C).", "To convert from a °C temperature");
+        System.out.printf("%-4s : %40s%n", "F).", "To convert from a °F temperature");
+        System.out.printf("%-4s : %40s%n", "K).", "To convert from a °K temperature");
+        System.out.printf("%-4s : %40s%n", "Q).", "To quit");
+        return input.nextLine();
+    }
+
+    public static String getUnitChoice2() { // An addition to make it easier to see what the second option is for.
+        System.out.printf("%-4s : %40s%n", "C).", "To convert to a °C temperature");
+        System.out.printf("%-4s : %40s%n", "F).", "To convert to a °F temperature");
+        System.out.printf("%-4s : %40s%n", "K).", "To convert to a °K temperature");
         System.out.printf("%-4s : %40s%n", "Q).", "To quit");
         return input.nextLine();
     }
@@ -39,6 +60,55 @@ public class TempConversion {
     public static void main(String[] args) {
         while(true) {
 
+            String convertFrom = getUnitChoice().toLowerCase(); // Looking back on this, it would've been easier to use .toUpperCase() here instead of on all the display areas.
+
+            if (convertFrom.equals("q")) { break; }
+
+            String convertTo = getUnitChoice2().toLowerCase();
+
+            if (convertTo.equals("q")) { break; }
+            if (convertFrom.equals(convertTo)) {
+                System.out.println("\nPlease choose two different units to convert.\n");
+                continue;
+            }
+
+            double initialTemp = getTemp(convertFrom);
+
+
+            switch (convertFrom) {
+                case "c":
+                    switch (convertTo) {
+                        case "f":
+                            System.out.printf("\nResult %f°%s is %f°%s\n\n", initialTemp, convertFrom.toUpperCase(), convertC2F(initialTemp), convertTo.toUpperCase());
+                            break;
+                        case "k":
+                            System.out.printf("\nResult %f°%s is %f°%s\n\n", initialTemp, convertFrom.toUpperCase(), convertC2K(initialTemp), convertTo.toUpperCase());
+                            break;
+                    }
+                    break;
+
+                case "f":
+                    switch (convertTo) {
+                        case "c":
+                            System.out.printf("\nResult %f°%s is %f°%s\n\n", initialTemp, convertFrom.toUpperCase(), convertF2C(initialTemp), convertTo.toUpperCase());
+                            break;
+                        case "k":
+                            System.out.printf("\nResult %f°%s is %f°%s\n\n", initialTemp, convertFrom.toUpperCase(), convertF2K(initialTemp), convertTo.toUpperCase());
+                            break;
+                    }
+                    break;
+
+                case "k":
+                    switch (convertTo) {
+                        case "c":
+                            System.out.printf("\nResult %f°%s is %f°%s\n\n", initialTemp, convertFrom, convertK2C(initialTemp), convertTo);
+                            break;
+                        case "f":
+                            System.out.printf("\nResult %f°%s is %f°%s\n\n", initialTemp, convertFrom, convertK2F(initialTemp), convertTo);
+                            break;
+                    }
+                    break;
+            }
         }
     }
 }
